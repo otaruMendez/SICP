5cf94f07684976e0bb52717b2f4105967746189d
Tunde Otaru <otarubabatunde@gmail.com>
2018-07-07 15:27:20 +0100

    Solve exercise 1.2

---
+++ chapter-1/ex-1.2.lisp
@@ -0,0 +1,18 @@
+Question:
+
+Translate the following expression into prefix form
+
+Answer:
+
+a = (/ 1 5)
+b = (+ 6 a)
+c = (-3 b)
+d = (- 2 c)
+e = (+ 5 (/ 1 4) d)
+f = (- 2 7)
+g = (- 6 2)
+h = (* 3 g f)
+
+result = (/ e h)
+
+= (/ (+ 5 (/ 1 4) (- 2 (-3 (+ 6 (/ 1 5))))) (* 3 (- 6 2) (- 2 7)))
