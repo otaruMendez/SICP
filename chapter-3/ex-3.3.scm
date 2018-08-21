41be2e16358687555082820efb55504d1020cbe1
Tunde Otaru <otarubabatunde@gmail.com>
2018-08-21 22:40:48 +0100

    Re format solution ex-3.3

--- chapter-3/ex-3.3.scm
+++ chapter-3/ex-3.3.scm
@@ -15,21 +15,21 @@
 
 
 ;MAKE-ACCOUNT FUNCTION FROM TEXT:
-#;(define (make-account balance)
-  (define (withdraw amount)
-    (if (>= balance amount)
-        (begin (set! balance (- balance amount))
-               balance)
-        "Insufficient funds"))
-  (define (deposit amount)
-    (set! balance (+ balance amount))
-    balance)
-  (define (dispatch m)
-    (cond ((eq? m ’withdraw) withdraw)
-          ((eq? m ’deposit) deposit)
-          (else (error "Unknown request -- MAKE-ACCOUNT"
-m))))
-dispatch)
+;(define (make-account balance)
+;(define (withdraw amount)
+;   (if (>= balance amount)
+;        (begin (set! balance (- balance amount))
+;               balance)
+;        "Insufficient funds"))
+;  (define (deposit amount)
+;    (set! balance (+ balance amount))
+;    balance)
+;  (define (dispatch m)
+;    (cond ((eq? m ’withdraw) withdraw)
+;          ((eq? m ’deposit) deposit)
+;          (else (error "Unknown request -- MAKE-ACCOUNT"
+;m))))
+;dispatch)
 
 
 ;ANSWER
