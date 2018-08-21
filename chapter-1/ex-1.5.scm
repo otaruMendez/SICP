ac9cd635b45f4ef282738b5e17dab88941ff1462
Tunde Otaru <otarubabatunde@gmail.com>
2018-08-21 09:11:47 +0100

    Solve exercise 1.5

---
+++ chapter-1/ex-1.5.scm
@@ -0,0 +1,8 @@
+;APPLICATIVE-ORDER EVALUATION
+;execution steps:
+; - if (x = 0) -> true
+; - result -> 0
+
+;NORMAL-ORDER EVALUATION
+;execution steps:
+; - execute procedure (p) -> runs infinitely
