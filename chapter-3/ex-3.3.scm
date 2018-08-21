1952ecd1bf2afb01eba7dfc9fec41b75756cb6ff
Tunde Otaru <otarubabatunde@gmail.com>
2018-08-21 22:36:28 +0100

    Format solution ex-3.3

--- chapter-3/ex-3.3.scm
+++ chapter-3/ex-3.3.scm
@@ -1,22 +1,21 @@
-#|
-QUESTION:
-Modify the make-account procedure so that it
-creates password-protected accounts.
-That is, make-account should take a symbol as an
-additional argument, as in
-(define acc (make-account 100 ’secret-password))
-The resulting account object should process a
-request only if it is accompanied by
-the password with which the account was created,
- and should otherwise return a complaint:
-((acc ’secret-password ’withdraw) 40)
-60
-((acc ’some-other-password ’deposit) 50)
-"Incorrect password"
+;QUESTION:
+;Modify the make-account procedure so that it
+;creates password-protected accounts.
+;That is, make-account should take a symbol as an
+;additional argument, as in
+;(define acc (make-account 100 ’secret-password))
+;The resulting account object should process a
+;request only if it is accompanied by
+;the password with which the account was created,
+;and should otherwise return a complaint:
+;((acc ’secret-password ’withdraw) 40)
+;60
+;((acc ’some-other-password ’deposit) 50)
+;"Incorrect password"
 
 
-MAKE-ACCOUNT FUNCTION FROM TEXT:
-(define (make-account balance)
+;MAKE-ACCOUNT FUNCTION FROM TEXT:
+#;(define (make-account balance)
   (define (withdraw amount)
     (if (>= balance amount)
         (begin (set! balance (- balance amount))
@@ -31,7 +30,7 @@ MAKE-ACCOUNT FUNCTION FROM TEXT:
           (else (error "Unknown request -- MAKE-ACCOUNT"
 m))))
 dispatch)
-|#
+
 
 ;ANSWER
 (define (make-account balance password)
